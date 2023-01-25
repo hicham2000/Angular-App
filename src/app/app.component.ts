b75ce527f12a5e361af74bb5a3d2eab177589970
hicham <oo906496@gmail.com>
2023-01-25 23:37:25 +0100

    control components from header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularApp';
+
+  section:string="recipe";
+
+  onrecive(sec:string){
+    this.section=sec;
+  }
 }
